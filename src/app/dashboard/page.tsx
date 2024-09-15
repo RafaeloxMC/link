94cdf8de6e7a7eddca07847f352f53bb782cc085
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2024-09-15 12:43:01 +0200

    improve dots bg and optimize sidebar in dash

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -33,7 +33,9 @@ export default function Dashboard() {
 
 	async function fetchLinks() {
 		try {
-			const response = await axios.get("/api/me", { withCredentials: true });
+			const response = await axios.get("/api/me", {
+				withCredentials: true,
+			});
 			setLinks(response.data.links);
 			setLinksLoaded(true);
 		} catch (error) {
@@ -46,13 +48,21 @@ export default function Dashboard() {
 	}, []);
 
 	function isCurrentValid() {
-		if (shortLink !== "" && longLink !== "" && created != undefined && totalClicks != undefined) return true;
+		if (
+			shortLink !== "" &&
+			longLink !== "" &&
+			created != undefined &&
+			totalClicks != undefined
+		)
+			return true;
 		return false;
 	}
 
 	async function deleteLink() {
 		try {
-			await axios.delete(`/api/link/${shortLink}`, { withCredentials: true });
+			await axios.delete(`/api/link/${shortLink}`, {
+				withCredentials: true,
+			});
 			setLinks(links.filter((link) => link.id !== shortLink));
 			setShortLink("");
 			setLongLink("");
@@ -74,12 +84,21 @@ export default function Dashboard() {
 
 	const handleCreateNewLink = async (event: React.FormEvent) => {
 		event.preventDefault();
-		if (modalShortLink === "" || modalLongLink === "") return setModalError("Please fill out the required information!");
+		if (modalShortLink === "" || modalLongLink === "")
+			return setModalError("Please fill out the required information!");
 		const allowedRegex = /^[a-zA-Z0-9-_]+$/;
-		if (!allowedRegex.test(modalShortLink)) return setModalError("Invalid characters in link name!");
-		if (modalShortLink.length > 20) return setModalError("LiNK too long! (> 20 characters)");
+		if (!allowedRegex.test(modalShortLink))
+			return setModalError("Invalid characters in link name!");
+		if (modalShortLink.length > 20)
+			return setModalError("LiNK too long! (> 20 characters)");
 		try {
-			await axios.post("/api/link/" + modalShortLink, { url: modalLongLink }, { withCredentials: true }).then((res) => fetchLinks());
+			await axios
+				.post(
+					"/api/link/" + modalShortLink,
+					{ url: modalLongLink },
+					{ withCredentials: true }
+				)
+				.then((res) => fetchLinks());
 			clearModal();
 		} catch (error) {
 			const axiosError = error as AxiosError;
@@ -104,78 +123,127 @@ export default function Dashboard() {
 
 			<div className="relative z-20 flex w-full h-screen p-4 gap-4">
 				<div className="w-1/4 bg-white bg-opacity-5 backdrop-blur-xl rounded-3xl flex flex-col p-4">
-					<h1 className="text-4xl text-white font-bold mb-4 text-center">Your LiNKs</h1>
+					<h1 className="text-4xl text-white font-bold mb-4 text-center">
+						Your LiNKs
+					</h1>
 					<div className="h-full flex flex-col justify-start gap-4">
 						{linksLoaded ? (
 							links && links.length > 0 ? (
 								links.map((link) => (
-									<button key={link.id} className="w-full p-2 text-white bg-gray-800 bg-opacity-50 rounded-3xl hover:bg-opacity-75" onClick={() => setData(link)}>
+									<button
+										key={link.id}
+										className="w-full p-2 text-white bg-white bg-opacity-20 rounded-3xl hover:bg-opacity-45 transition duration-300"
+										onClick={() => setData(link)}>
 										{link.id} - {link.url}
 									</button>
 								))
 							) : (
-								<p className="text-center text-xl mt-2">No links found!</p>
+								<p className="text-center text-xl mt-2">
+									No links found!
+								</p>
 							)
 						) : (
-							<p className="text-center text-xl mt-2">Loading...</p>
+							<p className="text-center text-xl mt-2">
+								Loading...
+							</p>
 						)}
 					</div>
-					<button className="w-full p-2 text-white bg-gray-800 bg-opacity-50 rounded-3xl hover:bg-opacity-75" onClick={() => setShowModal(true)}>
+					<hr className="ml-2 mr-2 rounded-full my-4 border-gray-600" />
+					<button
+						className="w-full p-2 text-white bg-white bg-opacity-20 rounded-3xl hover:bg-opacity-45 transition duration-300"
+						onClick={() => setShowModal(true)}>
 						Create new link
 					</button>
 				</div>
 
 				<div className="w-3/4 h-full flex flex-col items-center justify-center gap-5">
 					<ContentBox className="w-full h-full min-w-full max-w-full flex flex-col justify-between p-6 bg-gray-800 bg-opacity-10 rounded-2xl shadow-lg">
-						<h1 className="text-white text-3xl mb-2 border-b border-gray-600 pb-2 w-full">Link Information</h1>
+						<h1 className="text-white text-3xl mb-2 border-b border-gray-600 pb-2 w-full">
+							Link Information
+						</h1>
 						{isCurrentValid() ? (
 							<div className="h-full w-full min-w-full flex flex-col">
 								<div className="h-full flex flex-col gap-2 flex-grow">
 									<p className="text-gray-300">
 										Short link:{" "}
-										<span className="text-white cursor-pointer hover:underline" onClick={() => router.push(`https://l.xvcf.dev/${shortLink}`)}>
+										<span
+											className="text-white cursor-pointer hover:underline"
+											onClick={() =>
+												router.push(
+													`https://l.xvcf.dev/${shortLink}`
+												)
+											}>
 											https://l.xvcf.dev/{shortLink}
 										</span>
 									</p>
 									<p className="text-gray-300">
 										Redirects to:{" "}
-										<span className="text-white cursor-pointer hover:underline" onClick={() => router.push(longLink)}>
+										<span
+											className="text-white cursor-pointer hover:underline"
+											onClick={() =>
+												router.push(longLink)
+											}>
 											{longLink}
 										</span>
 									</p>
 									<p className="text-gray-300">
-										Created: <span className="text-white">{new Date(created).getTime() === 0 ? "never" : new Date(created).toLocaleString()}</span>
+										Created:{" "}
+										<span className="text-white">
+											{new Date(created).getTime() === 0
+												? "never"
+												: new Date(
+														created
+												  ).toLocaleString()}
+										</span>
 									</p>
 								</div>
 								<div className="border-t border-gray-600 pt-2 w-full mt-auto">
 									<div className="gap-2 w-full">
 										{/* Might be added in the future */}
 										{/* <button className="w-full p-2 text-white bg-gray-500 rounded-3xl hover:bg-opacity-75 backdrop-blur-lg bg-opacity-50">Edit</button> */}
-										<button className="w-full p-2 text-white bg-red-500 rounded-3xl hover:bg-opacity-75 backdrop-blur-lg bg-opacity-50" onClick={() => deleteLink()}>
+										<button
+											className="w-full p-2 text-white bg-red-500 rounded-3xl hover:bg-opacity-75 backdrop-blur-lg bg-opacity-50"
+											onClick={() => deleteLink()}>
 											Delete
 										</button>
 									</div>
 								</div>
 							</div>
 						) : (
-							<p className="text-left text-xl mt-2">Select a link to view information!</p>
+							<p className="text-left text-xl mt-2">
+								Select a link to view information!
+							</p>
 						)}
 					</ContentBox>
 					<ContentBox className="w-full h-full min-w-full max-w-full flex flex-col justify-start p-6 bg-gray-800 bg-opacity-10 rounded-2xl shadow-lg items-start">
-						<h1 className="text-white text-3xl mb-2 border-b border-gray-600 pb-2 w-full">Link Data</h1>
+						<h1 className="text-white text-3xl mb-2 border-b border-gray-600 pb-2 w-full">
+							Link Data
+						</h1>
 						{isCurrentValid() ? (
 							<div className="h-full w-full">
 								<div className="flex flex-col gap-2 flex-grow">
 									<p className="text-gray-300">
-										Total clicks: <span className="text-white">{totalClicks}</span>
+										Total clicks:{" "}
+										<span className="text-white">
+											{totalClicks}
+										</span>
 									</p>
 									<p className="text-gray-300">
-										Last click: <span className="text-white">{new Date(lastClick).getTime() === 0 ? "never" : new Date(lastClick).toLocaleString()}</span>
+										Last click:{" "}
+										<span className="text-white">
+											{new Date(lastClick).getTime() === 0
+												? "never"
+												: new Date(
+														lastClick
+												  ).toLocaleString()}
+										</span>
 									</p>
 								</div>
 							</div>
 						) : (
-							<p className="text-left text-xl mt-2">Select a link to view data!</p>
+							<p className="text-left text-xl mt-2">
+								Select a link to view data!
+							</p>
 						)}
 					</ContentBox>
 				</div>
@@ -187,29 +255,54 @@ export default function Dashboard() {
 						<h2 className="text-2xl mb-4">Create New LiNK</h2>
 						<form onSubmit={handleCreateNewLink}>
 							<div className="mb-4">
-								<label className="block text-white text-sm font-bold mb-2" htmlFor="shortLink">
+								<label
+									className="block text-white text-sm font-bold mb-2"
+									htmlFor="shortLink">
 									Your LiNK name
 								</label>
-								<input id="shortLink" type="text" value={modalShortLink} onChange={(e) => setModalShortLink(e.target.value)} className="w-full mb-4 p-3 rounded-full text-center border border-none focus:outline-none focus:ring-transparent text-white bg-white bg-opacity-10" />
+								<input
+									id="shortLink"
+									type="text"
+									value={modalShortLink}
+									onChange={(e) =>
+										setModalShortLink(e.target.value)
+									}
+									className="w-full mb-4 p-3 rounded-full text-center border border-none focus:outline-none focus:ring-transparent text-white bg-white bg-opacity-10"
+								/>
 							</div>
 							<div className="mb-4">
-								<label className="block text-white text-sm font-bold mb-2" htmlFor="longLink">
+								<label
+									className="block text-white text-sm font-bold mb-2"
+									htmlFor="longLink">
 									The target URL
 								</label>
-								<input id="longLink" type="text" value={modalLongLink} onChange={(e) => setModalLongLink(e.target.value)} className="w-full mb-4 p-3 rounded-full text-center border border-none focus:outline-none focus:ring-transparent text-white bg-white bg-opacity-10" />
+								<input
+									id="longLink"
+									type="text"
+									value={modalLongLink}
+									onChange={(e) =>
+										setModalLongLink(e.target.value)
+									}
+									className="w-full mb-4 p-3 rounded-full text-center border border-none focus:outline-none focus:ring-transparent text-white bg-white bg-opacity-10"
+								/>
 							</div>
-							{modalError && <p className="text-red-500 text-center mb-6 -mt-2">{modalError}</p>}
+							{modalError && (
+								<p className="text-red-500 text-center mb-6 -mt-2">
+									{modalError}
+								</p>
+							)}
 							<div className="flex flex-row gap-4 items-center justify-between">
 								<button
 									type="button"
 									className="text-white p-2 w-full bg-gray-800 bg-opacity-50 rounded-3xl hover:bg-opacity-75"
 									onClick={() => {
 										clearModal();
-									}}
-								>
+									}}>
 									Cancel
 								</button>
-								<button type="submit" className="text-white p-2 w-full bg-gray-800 bg-opacity-50 rounded-3xl hover:bg-opacity-75">
+								<button
+									type="submit"
+									className="text-white p-2 w-full bg-gray-800 bg-opacity-50 rounded-3xl hover:bg-opacity-75">
 									Create
 								</button>
 							</div>
--- src/components/MovingDotsBackground.tsx
+++ src/components/MovingDotsBackground.tsx
@@ -1,18 +1,30 @@
-import dotColors from "@/util/colors";
-import { movingDots } from "@/util/motion";
-import { motion } from "framer-motion";
 import { useMemo } from "react";
+import { motion } from "framer-motion";
+import { movingDots } from "@/util/motion";
+
+const colors = [
+	"bg-gradient-to-r from-blue-300 to-indigo-300",
+	"bg-gradient-to-r from-red-300 to-orange-300",
+	"bg-gradient-to-r from-green-300 to-teal-300",
+	"bg-gradient-to-r from-yellow-300 to-green-300",
+	"bg-gradient-to-r from-purple-300 to-pink-300",
+	"bg-gradient-to-r from-indigo-300 to-blue-300",
+	"bg-gradient-to-r from-orange-300 to-red-300",
+	"bg-gradient-to-r from-cyan-300 to-blue-300",
+	"bg-gradient-to-r from-lime-300 to-green-300",
+	"bg-gradient-to-r from-rose-300 to-pink-300",
+];
 
 export default function MovingDotsBackground() {
-	return useMemo(() => {
-		return (
+	return useMemo(
+		() => (
 			<div>
 				<div className="absolute inset-0 z-0 overflow-hidden">
-					{Array.from({ length: 5 }).map((_, i) => (
+					{Array.from({ length: 10 }).map((_, i) => (
 						<motion.div
 							key={i}
-							className={`absolute w-[75vh] h-[75vh] rounded-full ${
-								dotColors()[i % dotColors().length]
+							className={`absolute w-[50vh] aspect-square rounded-full ${
+								colors[i % colors.length]
 							} backdrop-blur-3xl`}
 							initial="hidden"
 							animate="visible"
@@ -20,9 +32,10 @@ export default function MovingDotsBackground() {
 						/>
 					))}
 				</div>
-	
-				<div className="absolute inset-0 z-10 backdrop-blur-xl" />
+
+				<div className="absolute inset-0 z-10 backdrop-blur-3xl" />
 			</div>
-		);
-	}, [])
+		),
+		[]
+	);
 }
